b560db889234a01dd3d6fc14e20b5a0324804955
Andres <amontbrun@yahoo.com>
2018-03-19 18:36:12 -0400

    Actualizacion

--- src/App.vue
+++ src/App.vue
@@ -269,7 +269,15 @@
     </div>
   </div>
 </template>
+    <!-- Bootstrap core JavaScript -->
+    <script src="./assets/vendor/jquery/jquery.min.js"></script>
+    <script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
+    <!-- Plugin JavaScript -->
+    <script src="./assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="./assets/js/resume.min.js"></script>
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
